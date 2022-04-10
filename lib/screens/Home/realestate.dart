b5278e81146b755a6f480cfdac258d4b3e551828
Ali Arfa <58394112+aliarfa123@users.noreply.github.com>
2022-04-11 01:56:36 +0500

    main

--- lib/screens/Home/realestate.dart
+++ lib/screens/Home/realestate.dart
@@ -16,6 +16,7 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     readData();
+    readImage();
   }
 
   String? searchKey;
@@ -43,7 +44,112 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  readImage() {
+    DatabaseReference databaseref = FirebaseDatabase.instance.ref('Addresses');
+    databaseref.onValue.listen((DatabaseEvent event) {
+      final data = event.snapshot.value;
+      setState(() {
+        data1 = data;
+        p1 = data1;
+      });
+      p1!.forEach((key, values) {
+        setState(() {
+          imageList.add(values['image_link']);
+        });
+      });
+      // setState(() {
+      //   newDataList = newList;
+      // });
+      print(imageList);
+    });
+  }
+
+  readName() {
+    DatabaseReference databaseref = FirebaseDatabase.instance.ref('Addresses');
+    databaseref.onValue.listen((DatabaseEvent event) {
+      final data = event.snapshot.value;
+      setState(() {
+        data1 = data;
+        p1 = data1;
+      });
+      p1!.forEach((key, values) {
+        setState(() {
+          nameList.add(values['Name']);
+        });
+      });
+      // setState(() {
+      //   newDataList = newList;
+      // });
+      print(imageList);
+    });
+  }
+
+  readEmail() {
+    DatabaseReference databaseref = FirebaseDatabase.instance.ref('Addresses');
+    databaseref.onValue.listen((DatabaseEvent event) {
+      final data = event.snapshot.value;
+      setState(() {
+        data1 = data;
+        p1 = data1;
+      });
+      p1!.forEach((key, values) {
+        setState(() {
+          emailList.add(values['Email']);
+        });
+      });
+      // setState(() {
+      //   newDataList = newList;
+      // });
+      print(imageList);
+    });
+  }
+
+  readContact() {
+    DatabaseReference databaseref = FirebaseDatabase.instance.ref('Addresses');
+    databaseref.onValue.listen((DatabaseEvent event) {
+      final data = event.snapshot.value;
+      setState(() {
+        data1 = data;
+        p1 = data1;
+      });
+      p1!.forEach((key, values) {
+        setState(() {
+          contactList.add(values['Contact']);
+        });
+      });
+      // setState(() {
+      //   newDataList = newList;
+      // });
+      print(imageList);
+    });
+  }
+
+  readTel() {
+    DatabaseReference databaseref = FirebaseDatabase.instance.ref('Addresses');
+    databaseref.onValue.listen((DatabaseEvent event) {
+      final data = event.snapshot.value;
+      setState(() {
+        data1 = data;
+        p1 = data1;
+      });
+      p1!.forEach((key, values) {
+        setState(() {
+          telList.add(values['Tel']);
+        });
+      });
+      // setState(() {
+      //   newDataList = newList;
+      // });
+      print(imageList);
+    });
+  }
+
   List<String> newDataList = [];
+  List<String> nameList = [];
+  List<String> contactList = [];
+  List<String> emailList = [];
+  List<String> telList = [];
+  List<String> imageList = [];
   onItemChanged(String value) {
     setState(() {
       newDataList = newList
@@ -71,128 +177,163 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Column(
         children: [
-          Expanded(
-            child: StreamBuilder(
-              stream: FirebaseDatabase.instance
-                  .ref('Addresses')
-                  .orderByKey()
-                  .onValue,
-              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-                final tilesList = <Widget>[];
-                if (snapshot.hasData) {
-                  final address = Map<String, dynamic>.from(
-                      (snapshot.data!).snapshot.value);
-                  address.forEach((key, value) {
-                    final nextAdress = Map<String, dynamic>.from(value);
-                    final adressTile = Padding(
-                      padding: EdgeInsets.all(9),
-                      child: Column(
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              // readData();
-
-                              // print(address);
-                              // setState(() {
-                              //   addressSearch = nextAdress;
-                              //   print(addressSearch);
-                              // });
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => PropertyDetail(
-                                    contact: nextAdress['Contact'].toString(),
-                                    email: nextAdress['Email'].toString(),
-                                    name: nextAdress['Name'].toString(),
-                                    tel: nextAdress['Tel'].toString(),
-                                    image: nextAdress['image_link'].toString(),
-                                    address: nextAdress['Address'].toString(),
-                                  ),
-                                ),
-                              );
-                            },
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  border: Border.all(color: Colors.grey),
-                                  borderRadius: BorderRadius.circular(10),
-                                  boxShadow: [
-                                    BoxShadow(
-                                      color: Colors.grey.withOpacity(0.5),
-                                      blurRadius: 4,
-                                      offset: Offset(0, 4),
-                                    )
-                                  ]),
-                              height: size.height * 0.11,
-                              child: Center(
-                                child: ListTile(
-                                  leading: Image(
-                                    image: NetworkImage(
-                                      nextAdress['image_link'].toString(),
-                                    ),
-                                  ),
-                                  title: Text(
-                                    nextAdress['Address'].toString(),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                    tilesList.add(adressTile);
-                  });
-                }
-                return Column(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.symmetric(vertical: 10),
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 20,
-                      ),
-                      width: size.width * 0.95,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: primary),
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(29),
-                      ),
-                      child: TextField(
-                        controller: search,
-                        // onChanged: (value) {
-                        //   setState(() {
-                        //     newDataList = newList
-                        //         .where((string) => string
-                        //             .toLowerCase()
-                        //             .contains(value.toLowerCase()))
-                        //         .toList();
-                        //   });
-                        // },
-                        style: TextStyle(color: primary),
-                        cursorColor: primary,
-                        decoration: InputDecoration(
-                          icon: Icon(
-                            Icons.search,
-                            color: primary,
-                          ),
-                          hintText: 'Search',
-                          // hintStyle: TextStyle(
-                          //   color: primary,
-                          // ),
-                          border: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: ListView(
-                        children: tilesList,
-                      ),
-                    ),
-                  ],
-                );
-              },
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            width: size.width * 0.95,
+            decoration: BoxDecoration(
+              border: Border.all(color: primary),
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(29),
+            ),
+            child: TextField(
+              onChanged: onItemChanged,
+              controller: search,
+              // onChanged: (value) {
+              //   setState(() {
+              //     newDataList = newList
+              //         .where((string) => string
+              //             .toLowerCase()
+              //             .contains(value.toLowerCase()))
+              //         .toList();
+              //   });
+              // },
+              style: TextStyle(color: primary),
+              cursorColor: primary,
+              decoration: InputDecoration(
+                icon: Icon(
+                  Icons.search,
+                  color: primary,
+                ),
+                hintText: 'Search',
+                // hintStyle: TextStyle(
+                //   color: primary,
+                // ),
+                border: InputBorder.none,
+              ),
             ),
           ),
+          Column(
+            children: List.generate(
+              newDataList.length,
+              (index) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          blurRadius: 4,
+                          offset: Offset(0, 4),
+                        )
+                      ]),
+                  height: size.height * 0.11,
+                  child: Center(
+                    child: ListTile(
+                      leading: Image(
+                        image: NetworkImage(
+                          imageList[index],
+                        ),
+                      ),
+                      title: Text(
+                        newDataList[index],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // Expanded(
+          //   child: StreamBuilder(
+          //     stream: FirebaseDatabase.instance
+          //         .ref('Addresses')
+          //         .orderByKey()
+          //         .onValue,
+          //     builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          //       final tilesList = <Widget>[];
+          //       if (snapshot.hasData) {
+          //         final address = Map<String, dynamic>.from(
+          //             (snapshot.data!).snapshot.value);
+          //         address.forEach((key, value) {
+          //           final nextAdress = Map<String, dynamic>.from(value);
+          //           final adressTile = Padding(
+          //             padding: EdgeInsets.all(9),
+          //             child: Column(
+          //               children: [
+          //                 GestureDetector(
+          //                   onTap: () {
+          //                     // readData();
+
+          //                     // print(address);
+          //                     // setState(() {
+          //                     //   addressSearch = nextAdress;
+          //                     //   print(addressSearch);
+          //                     // });
+          //                     Navigator.push(
+          //                       context,
+          //                       MaterialPageRoute(
+          //                         builder: (context) => PropertyDetail(
+          //                           contact: nextAdress['Contact'].toString(),
+          //                           email: nextAdress['Email'].toString(),
+          //                           name: nextAdress['Name'].toString(),
+          //                           tel: nextAdress['Tel'].toString(),
+          //                           image: nextAdress['image_link'].toString(),
+          //                           address: nextAdress['Address'].toString(),
+          //                         ),
+          //                       ),
+          //                     );
+          //                   },
+          //                   child: Container(
+          //                     decoration: BoxDecoration(
+          //                         color: Colors.white,
+          //                         border: Border.all(color: Colors.grey),
+          //                         borderRadius: BorderRadius.circular(10),
+          //                         boxShadow: [
+          //                           BoxShadow(
+          //                             color: Colors.grey.withOpacity(0.5),
+          //                             blurRadius: 4,
+          //                             offset: Offset(0, 4),
+          //                           )
+          //                         ]),
+          //                     height: size.height * 0.11,
+          //                     child: Center(
+          //                       child: ListTile(
+          //                         leading: Image(
+          //                           image: NetworkImage(
+          //                             nextAdress['image_link'].toString(),
+          //                           ),
+          //                         ),
+          //                         title: Text(
+          //                           nextAdress['Address'].toString(),
+          //                         ),
+          //                       ),
+          //                     ),
+          //                   ),
+          //                 ),
+          //               ],
+          //             ),
+          //           );
+          //           tilesList.add(adressTile);
+          //         });
+          //       }
+          //       return Column(
+          //         children: [
+          //           Expanded(
+          //             child: ListView(
+          //               children: tilesList,
+          //             ),
+          //           ),
+          //         ],
+          //       );
+          //     },
+          //   ),
+          // ),
         ],
       ),
     );
